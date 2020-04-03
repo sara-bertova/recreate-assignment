c45e0633386c22865ea8c59ac9b12fae8667d489
sara-bertova <57212661+sara-bertova@users.noreply.github.com>
2020-04-03 13:20:40 +0200

    colours

--- js.js
+++ js.js
@@ -7,8 +7,8 @@ fetch("http://rasbery.eu/cms/wp-json/wp/v2/bike")
 })
 
 function showData(jsonData){
-    console.log(jsonData)
     jsonData.forEach(showBike)
+
 }
 
 
@@ -17,8 +17,23 @@ function showBike(bike) {
     const clone = template.cloneNode(true);
     clone.querySelector("h1").textContent = bike.title.rendered;
     clone.querySelector("h3").textContent = bike.brand;
-    clone.querySelector(".price span").textContent = "$" + `${bike.price}`;
+    clone.querySelector(".price span").textContent = `${bike.price}`;
     clone.querySelector(".colours span").textContent = bike.colours;
+
+    const colArray = bike.colours.split(",");
+    console.log(colArray)
+    colArray.forEach(color=>{
+        const col = document.createElement("div");
+        col.style.background = color;
+        clone.querySelector(".colors").appendChild(col);
+
+    })
+
+    if (bike.colours === "N/A"){
+        clone.querySelector(".colours").style.display = "block";
+        clone.querySelector(".colors").style.display = "none";
+    }
+
     clone.querySelector(".status span").textContent = bike.stock;
     clone.querySelector("img").src=bike.images.guid;
     document.querySelector("#bikestock").appendChild(clone);
